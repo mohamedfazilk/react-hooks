2541ad3fd794df3fade7b857de2936804aad96aa
mohamedfazilk <2114921@cognizant.com>
2022-08-31 10:31:31 +0530

    place the dummy data and create another folder AnimalCard

---
+++ src/components/Animalcard/AnimalCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './AnimalCard.css'
+
+const AnimalCard = () => {
+  return (
+    <div>
+      <h2>  Animal Cards</h2>
+    </div>
+  )
+}
+
+export default AnimalCard
+
