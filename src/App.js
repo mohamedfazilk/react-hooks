8fa57309ecdf0324d144d490d3419ac1d97a3cb5
mohamedfazilk <2114921@cognizant.com>
2022-08-31 11:01:55 +0530

    creating a  function for to show additional info

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import data from "./components/data";
 import AnimalCard from "./components/Animalcard/AnimalCard";
 
 function App() {
+  const alertInformation = Object.entries(additional)
+  .map(information => `${information[0]}: ${information[1]}`)
+  .join('\n');
+ alert(alertInformation)
+
   return (
     <div className="wrapper">
       <h1>Animals</h1>
       {data.map((animal)=>{
         return(
           <AnimalCard key={animal.name}
-          name={animal.name}/>
+          name={animal.name}
+          scientificName={animal.scientificName} 
+          size={animal.size}
+          diet={animal.diet}/>
         )
 
       })}
--- src/components/Animalcard/AnimalCard.js
+++ src/components/Animalcard/AnimalCard.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import './AnimalCard.css'
 
-const AnimalCard = (props) => {
-    const {name} = props
-  return (
-    <div>
-      <h2>  {name}</h2>
-    </div>
-  )
+const AnimalCard = ({
+    additional,
+    diet,
+    name,
+    scientificName,
+    size
+}) => {
+
+    return (
+        <div>
+            <h2>  {name}</h2>
+            <h3>{scientificName}</h3>
+            <h4>{size}kg</h4>
+            <div>{diet.join(', ')}.</div>
+
+        </div>
+    )
 }
 
 export default AnimalCard
